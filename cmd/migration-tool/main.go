b9bada0a1d9d1eaa669aff35a31ae175af816dc1
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-12-04 14:05:40 +0100

    Create main.go

---
+++ cmd/migration-tool/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/wissemmansouri/OpenIT.one-AppManagement/common"
+	interfaces "github.com/wisemmansouri/OpenIT.one-Common"
+	"github.com/wissemmansouri/OpenIT.one-Common/utils/systemctl"
+)
+
+const (
+	appManagementConfigDirPath  = "/etc/openit"
+	appManagementConfigFilePath = "/etc/openit/app-management.conf"
+	appManagementName           = "openit-app-management.service"
+	appManagementNameShort      = "app-management"
+)
+
+//go:embedded ../../build/sysroot/etc/openit/app-management.conf.sample
+//var _appManagementConfigFileSample string
+
+var (
+	_logger *Logger
+	// _status *version.GlobalMigrationStatus
+
+	commit = "private build"
+	date   = "private build"
+)
+
+func main() {
+	versionFlag := flag.Bool("v", false, "version")
+	debugFlag := flag.Bool("d", true, "debug")
+	forceFlag := flag.Bool("f", false, "force")
+	flag.Parse()
+
+	if *versionFlag {
+		fmt.Printf("v%s\n", common.AppManagementVersion)
+		os.Exit(0)
+	}
+
+	println("git commit:", commit)
+	println("build date:", date)
+
+	_logger = NewLogger()
+
+	if os.Getuid() != 0 {
+		_logger.Info("Root privileges are required to run this program.")
+		os.Exit(1)
+	}
+
+	if *debugFlag {
+		_logger.DebugMode = true
+	}
+
+	if !*forceFlag {
+		isRunning, err := systemctl.IsServiceRunning(appManagementName)
+		if err != nil {
+			_logger.Error("Failed to check if %s is running", appManagementName)
+			panic(err)
+		}
+
+		if isRunning {
+			_logger.Info("%s is running. If migration is still needed, try with -f.", appManagementName)
+			os.Exit(1)
+		}
+	}
+
+	migrationTools := []interfaces.MigrationTool{
+		// NewMigrationDummy(),
+	}
+
+	var selectedMigrationTool interfaces.MigrationTool
+
+	// look for the right migration tool matching current version
+	for _, tool := range migrationTools {
+		migrationNeeded, _ := tool.IsMigrationNeeded()
+
+		if migrationNeeded {
+			selectedMigrationTool = tool
+			break
+		}
+	}
+
+	if selectedMigrationTool == nil {
+		_logger.Info("No migration to proceed.")
+		return
+	}
+
+	if err := selectedMigrationTool.PreMigrate(); err != nil {
+		panic(err)
+	}
+
+	if err := selectedMigrationTool.Migrate(); err != nil {
+		panic(err)
+	}
+
+	if err := selectedMigrationTool.PostMigrate(); err != nil {
+		_logger.Error("Migration succeeded, but post-migration failed: %s", err)
+	}
+}
